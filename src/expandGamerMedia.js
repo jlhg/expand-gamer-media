a85d43f3486944b2561ee05b93b2d8f3da45328f
Jian-Long Huang <huang@jianlong.org>
2017-08-16 23:39:40 +0800

    Upgrade version

--- src/expandGamerMedia.js
+++ src/expandGamerMedia.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         巴哈姆特自動開圖
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  自動展開巴哈姆特哈拉區文章內的圖片和影片
 // @author       Jian-Long Huang
 // @match        https://forum.gamer.com.tw/C.php*
