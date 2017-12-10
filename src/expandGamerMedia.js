6192dc5911ce9f3dd99d6530d0ced1d7ce9673ea
Jian-Long Huang <huang@jianlong.org>
2017-12-10 16:51:46 +0800

    Support mobile version

--- src/expandGamerMedia.js
+++ src/expandGamerMedia.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         巴哈姆特自動開圖
 // @namespace    http://tampermonkey.net/
-// @version      1.4
+// @version      1.5
 // @description  自動展開巴哈姆特哈拉區文章內的圖片和影片
 // @author       Jian-Long Huang
 // @match        https://forum.gamer.com.tw/*.php*
+// @match        https://m.gamer.com.tw/forum/C.php*
 // @grant        none
 // ==/UserScript==
 
@@ -27,6 +28,15 @@
     attachMov = attachMovs[i];
     if (attachMov.innerHTML.match('請點選觀看影片') || attachMov.innerHTML.match('開啟影片')) {
       attachMov.click();
+    }
+  }
+
+  // 手機版
+  attachImgs = document.getElementsByName('openimg');
+  for (i = 0; i < attachImgs.length; ++i) {
+    attachImg = attachImgs[i];
+    if (attachImg.innerHTML.match('開啟本頁圖片')) {
+      attachImg.getElementsByTagName('font')[0].click();
     }
   }
 })();
